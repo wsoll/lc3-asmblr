76127dd7489dc304471b19063c4f297eb36e7cb0
wsoll <lukasz.wsol@protonmail.com>
2024-05-23 11:26:29 +0200

    refactor: remove stale codebase, leave legacy on branch

--- src/run.py
+++ src/run.py
@@ -1,48 +1,22 @@
-import os
-
 from assembler import Assembler
 
 
-def load_file():
-    fn = "none"
-    while fn and not os.path.exists(fn):
-        print("\n".join(fn for fn in os.listdir(os.curdir) if fn.endswith(".asm")))
-        fn = input("Give name of .asm file: ")
-    with open(fn) as codefile:
-        asm_code = codefile.read()
-    return asm_code
+def load_assembly(filepath):
+    with open(filepath, "r") as file:
+        for line in file:
+            yield line
 
 
-def save_to_file(output):
-    # ToDo: ask for output filename
-    with open("out.obj", "wb") as f:
-        f.write(output)
-
-
-def produce_output(swap, memory, pc, orig):
-    memory[pc] = orig
-    if swap:
-        memory.byteswap()
-    output = memory[pc : pc + 1].tobytes()
-    output += memory[orig:pc].tobytes()
-    return output
-
-
-def main(assembly):
+def main(filepath):
     assembler = Assembler()
 
-    for line in assembly.splitlines():
-        result = assembler.step(line)
-        if not result:
-            break
+    for line in load_assembly("..."):
+        assembler.read(line)
 
-    assembler.link_labels_def_to_labels_usage()
-    return produce_output(
-        assembler.swap, assembler.memory, assembler.program_counter, assembler.origin
-    )
+    assembler.to_bytes()
+    # ToDo: Save to file
 
 
 if __name__ == "__main__":
-    assembly = load_file()
-    result = main(assembly)
-    save_to_file(result)
+    # ToDo: CLI for: filepath, verbose
+    main("...")
