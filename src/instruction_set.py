0538fa325a8bda7c8abd9d11a69ae17b8cd4f82e
wsoll <lukasz.wsol@protonmail.com>
2024-06-06 15:27:21 +0200

    refactor(instruction_set): unnecessary single-point of exit for opcodes match/case

--- src/instruction_set.py
+++ src/instruction_set.py
@@ -5,16 +5,15 @@ from syntax import is_numeral_base_prefixed, is_valid_goto_label, cast_to_numera
 class InstructionSet:
 
     def process_operands(self, operation_code: str, operands: list[str]) -> int:
-        operands_binary_repr = 0x000
         match operation_code:
             case OpCode.ADD:
-                operands_binary_repr |= self.get_add_or_and_operands_encoding(operands)
+                return self.get_add_or_and_operands_encoding(operands)
             case OpCode.AND:
-                operands_binary_repr |= self.get_add_or_and_operands_encoding(operands)
+                return self.get_add_or_and_operands_encoding(operands)
             case OpCode.BR:
                 ...
             case OpCode.JMP:
-                operands_binary_repr |= self.get_jump_operand_encoding(operands)
+                return self.get_jump_operand_encoding(operands)
             case OpCode.JSR:
                 ...
             case OpCode.JSRR:
@@ -28,10 +27,11 @@ class InstructionSet:
             case OpCode.LEA:
                 ...
             case OpCode.NOT:
-                operands_binary_repr |= self.get_operands_encoding(operands)
+                return self.get_not_operand_encoding(operands)
             case OpCode.RET:
                 if len(operands) != 0:
                     raise SyntaxError("Return instruction doesn't accept operands.")
+                return 0
             case OpCode.RTI:
                 ...
             case OpCode.ST:
@@ -40,7 +40,6 @@ class InstructionSet:
                 ...
             case OpCode.STR:
                 ...
-        return operands_binary_repr
 
     def get_operands_encoding(self, operands: list[str]) -> int:
         operands_encoding = 0
